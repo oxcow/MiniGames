9f96f14904a312576eba73f5da1f7e73e7ea9619
William.Li <william.li@activenetwork.com>
2023-05-16 18:01:39 +0800

    Fixed eslint warning

--- src/games/tetris/new/Canvas.jsx
+++ src/games/tetris/new/Canvas.jsx
@@ -1,119 +1,122 @@
-import { useEffect, useRef, useState } from "react";
-import useInterval from "../../../hooks/hooks";
-import { Point, TetrisI, TetrisJ, TetrisL, TetrisO, TetrisS, TetrisZ } from "./test";
+import { useEffect, useState } from 'react'
+import { Point, TetrisL } from './test'
+import useInterval from '../../../hooks/hooks'
 
 const initDataMatrix = () => {
-  const _dataMatrix = [];
+  const _dataMatrix = []
   for (let i = 19; i >= 0; i--) {
-    _dataMatrix.push(new Array(10).fill(0));
+    _dataMatrix.push(new Array(10).fill(0))
   }
-  console.log(_dataMatrix);
-  return _dataMatrix;
+  console.log(_dataMatrix)
+  return _dataMatrix
 }
 const Canvas = (props) => {
-  const [isStart, setStart] = useState(false);
-  const [refPoint, setRefPoint] = useState(new Point(3, 19));
-  const [dataMatrix, updateDataMatrix] = useState(() => initDataMatrix());
-  const [tileCoords, setCurrentTileCoords] = useState(null);
-  const [timerId, setTimerId] = useState(null);
-  const [tetris, setCurrentTetris] = useState(() => new TetrisL());
+  const [isStart, setStart] = useState(false)
+  const [refPoint, setRefPoint] = useState(new Point(3, 19))
+  const [dataMatrix, updateDataMatrix] = useState(() => initDataMatrix())
+  //const [tileCoords, setCurrentTileCoords] = useState(null);
+  const [timerId, setTimerId] = useState(null)
+  const tetris = new TetrisL()
 
   const move = () => {
     if (!tetris) {
-      return;
+      return
     }
 
-    const _matrix = [...dataMatrix];
+    const _matrix = [...dataMatrix]
 
-    const oldTileCoords = tetris.coordinate(refPoint);
+    const oldTileCoords = tetris.coordinate(refPoint)
     for (const ties of oldTileCoords) {
       if (ties.y < 20 && ties.x > -1 && ties.x < 10) {
-        _matrix[19 - ties.y][ties.x] = 0;
+        _matrix[19 - ties.y][ties.x] = 0
       }
     }
-    const newPoint = new Point(refPoint.x, refPoint.y - 1);
-    const newTileCoords = tetris.coordinate(newPoint);
+    const newPoint = new Point(refPoint.x, refPoint.y - 1)
+    const newTileCoords = tetris.coordinate(newPoint)
     for (const ties of newTileCoords) {
       if (ties.y < 20 && ties.x > -1 && ties.x < 10) {
-        _matrix[19 - ties.y][ties.x] = ties.v;
+        _matrix[19 - ties.y][ties.x] = ties.v
       }
     }
 
-    setRefPoint(newPoint);
-    setCurrentTileCoords(() => newTileCoords);
+    setRefPoint(newPoint)
+    //setCurrentTileCoords(() => newTileCoords);
 
-    updateDataMatrix(() => _matrix);
+    updateDataMatrix(() => _matrix)
 
   }
 
-  // const id = useInterval(()=>{
-  //   move();
-  // },1000);
-  // setTimerId(id);
+  const id = useInterval(() => {
+    move()
+  }, 1000)
+  setTimerId(id)
 
-  // useEffect(()=>{
-    
-  // },[]);
+  useEffect(() => {
+
+  }, [])
 
   useEffect(() => {
     if (isStart) {
-      move();
+      //move()
     } else {
       if (timerId) {
-        clearTimeout(timerId);
-        setTimerId(null);
+        //clearTimeout(timerId);
+        setTimerId(null)
       }
     }
 
-  }, [isStart]);
+  }, [isStart, timerId])
 
   const handlerStart = (e) => {
-    setStart(true);
+    setStart(true)
   }
 
   const handleStop = (e) => {
-    setStart(false);
+    setStart(false)
   }
 
   document.onkeydown = (e) => {
-    e.preventDefault();
+    e.preventDefault()
     switch (e.code) {
-      case "KeyS":
-      case "ArrowDown":
-        setRefPoint(() => new Point(refPoint.x, refPoint.y - 1));
-        break;
-      case "KeyW":
-      case "ArrowUp":
-        tetris.rotate();
-        break;
-      case "KeyA":
-      case "ArrowLeft":
-        setRefPoint(() => new Point(refPoint.x - 1, refPoint.y));
-        break;
-      case "KeyD":
-      case "ArrowRight":
-        setRefPoint(() => new Point(refPoint.x + 1, refPoint.y));
-        break;
+      case 'KeyS':
+      case 'ArrowDown':
+        setRefPoint(() => new Point(refPoint.x, refPoint.y - 1))
+        break
+      case 'KeyW':
+      case 'ArrowUp':
+        tetris.rotate()
+        break
+      case 'KeyA':
+      case 'ArrowLeft':
+        setRefPoint(() => new Point(refPoint.x - 1, refPoint.y))
+        break
+      case 'KeyD':
+      case 'ArrowRight':
+        setRefPoint(() => new Point(refPoint.x + 1, refPoint.y))
+        break
+      default:
     }
 
-  };
+  }
   return (
-    <div className="canvas">
-      <h1>{timerId}</h1>
-      <button onClick={handlerStart}>Start</button>
-      <button onClick={handleStop}>Stop</button>
-      {
-        dataMatrix.map((row, idx) => (idx < 19 && (
-          <div className='columns' key={idx}>
-            {row.map((col, offset) => (
-              <div className={`sharp s-rounded ${col ? 'bg-dark' : 'bg-gray'}`} key={offset}></div>
-            ))}
-          </div>
-        ))
-        )
-      }
-    </div>
+      <div className="canvas">
+        <h1>{timerId}</h1>
+        <button onClick={handlerStart}>Start</button>
+        <button onClick={handleStop}>Stop</button>
+        {
+          dataMatrix.map((row, idx) => (idx < 19 && (
+                  <div className="columns" key={idx}>
+                    {row.map((col, offset) => (
+                        <div className={`sharp s-rounded ${col
+                            ? 'bg-dark'
+                            : 'bg-gray'}`} key={offset}></div>
+                    ))}
+                  </div>
+              )),
+          )
+        }
+      </div>
   )
 }
 
-export default Canvas;
+export default Canvas
